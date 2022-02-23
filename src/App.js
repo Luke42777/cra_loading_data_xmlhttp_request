7db8f2126429aac60470a7f10ccfe05328776463
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-02-23 20:13:31 +0000

    working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import React, { Component } from 'react'
+import './App.css'
 
-import './App.css';
+class App extends Component {
+  state = {
+    users: [],
+  }
+  componentDidMount() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/1/comments', true);
+    xhr.onload = () => {
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+      if (xhr.status === 200) {
+        const users = JSON.parse(xhr.response);
+        console.log(users);
+
+        this.setState({
+          users,
+        })
+      }
+      console.log(typeof xhr.response);
+
+    }
+
+    xhr.send(null)
+  }
+
+  render() {
+    const users = this.state.users.map(user => (
+      <li key={user.id}><strong>Name: </strong>{user.name}
+       <strong>email: </strong>{user.email}</li>
+    ));
+    return (<ul>
+      {users}
+    </ul>)
+  }
 }
 
 export default App;
